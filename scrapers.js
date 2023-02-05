477722fe8f479272cd26865d69b7bb5a961cf379
Dane Buckley <danebuckley@live.com>
2023-02-04 20:43:41 -0800

    lists components as well

--- scrapers.js
+++ scrapers.js
@@ -1,42 +1,5 @@
-// const puppeteer = require('puppeteer');
-// const readline = require('readline')
-
-const { it } = require("node:test");
-
-// async function scrapeProduct(url) {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto(url);
-
-//     const [iOne] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[1]/div[2]/p');
-//     const itemOne = await iOne.getProperty('textContent');
-//     const itemOneTxt = await itemOne.jsonValue();
-
-//     const [iTwo] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[2]/div[2]/p');
-//     const itemTwo = await iTwo.getProperty('textContent');
-//     const itemTwoTxt = await itemTwo.jsonValue();
-
-//     const [iThree] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[3]/div[2]/p');
-//     const itemThree = await iThree.getProperty('textContent');
-//     const itemThreeTxt = await itemThree.jsonValue();
-
-//     console.log({itemOneTxt, itemTwoTxt, itemThreeTxt});
-//     browser.close();
-// }
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-
-// let userInput = "";
-
-// rl.question("What champion?\n", function (string) {
-//     userInput = string;
-//     scrapeProduct("https://app.mobalytics.gg/tft/champions/" + userInput);
-//     rl.close();
-// });
-
+const puppeteer = require('puppeteer');
+const readline = require('readline')
 
 const itemMap = new Map();
 
@@ -85,6 +48,59 @@ itemMap.set("Theif's Gloves", "Glove Glove");
 itemMap.set('Renegade Emblem', "Glove Spatula");
 itemMap.set('Anima Squad Emblem', "Rod Spatula");
 
+async function scrapeProduct(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-console.log(map1.get('a'))
+    const [iOne] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[1]/div[2]/p');
+    const itemOne = await iOne.getProperty('textContent');
+    const itemOneTxt = await itemOne.jsonValue();
+
+    const [iTwo] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[2]/div[2]/p');
+    const itemTwo = await iTwo.getProperty('textContent');
+    const itemTwoTxt = await itemTwo.jsonValue();
+
+    const [iThree] = await page.$x('//*[@id="container"]/div/main/div/div/div[2]/div[1]/div[4]/div/div/div[3]/div[2]/p');
+    const itemThree = await iThree.getProperty('textContent');
+    const itemThreeTxt = await itemThree.jsonValue();
+
+
+
+
+    console.log({itemOneTxt, itemTwoTxt, itemThreeTxt});
+
+    let componentsOne = itemMap.get(itemOneTxt);
+    let componentsTwo = itemMap.get(itemTwoTxt);
+    let componentsThree = itemMap.get(itemThreeTxt);
+
+    const arrayOne = componentsOne.split(" ");
+    const arrayTwo = componentsTwo.split(" ");
+    const arrayThree = componentsThree.split(" ");
+    
+    console.log(arrayOne[0], arrayOne[1]);
+    console.log(arrayTwo[0], arrayTwo[1]);
+    console.log(arrayThree[0], arrayThree[1]);
+
+    
+
+    browser.close();
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let userInput = "";
+
+rl.question("What champion?\n", function (string) {
+    userInput = string;
+    scrapeProduct("https://app.mobalytics.gg/tft/champions/" + userInput);
+    rl.close();
+});
+
+
+
+
 
