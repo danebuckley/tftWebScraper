88ff936e0ae74f00797fac739c1ec55b7769e313
Dane Buckley <danebuckley@live.com>
2023-02-05 17:30:04 -0800

    itemMap IDs added

--- scrapers.js
+++ scrapers.js
@@ -4,6 +4,50 @@ const { EventEmitter } = require('stream');
 
 const itemMap = new Map();
 const champList = new Array("Alistar", "Annie", "Aphelios", "Ashe", "aurelionsol", "Belveth", "Blitzcrank", "Camille", "Chogath", "Draven", "Ekko", "Ezreal", "Fiddlesticks", "Fiora", "Galio", "Gangplank", "Janna", "Jax", "Jinx", "Kaisa", "Kayle", "Leblanc", "Leesin", "Leona", "lulu", "lux", "malphite", "missfortune", "mordekaiser", "nasus", "nilah", "nunu", "poppy", "rammus", "rell", "renekton", "riven", "samira", "sejuani", "senna", "sett", "sivir", "sona", "soraka", "sylas", "syndra", "taliyah", "talon", "urgot", "vayne", "velkoz", "vi", "viego", "wukong", "yasuo", "yuumi", "zac", "zed", "zoe");
+itemMap.set(1054, 'Deathblade');
+itemMap.set(823, 'Giant Slayer');
+itemMap.set(945, 'Edge of Night');
+itemMap.set(820, 'Hextech Gunblade');
+itemMap.set(1017, 'Bloodthirster');
+itemMap.set(918, "Zeke's Herald");
+itemMap.set(1036, 'Infinity Edge');
+itemMap.set(1257, 'LaserCorps Emblem');
+itemMap.set(592, 'Rapid Firecannon');
+itemMap.set(714, "Titan's Resolve");
+itemMap.set(589, "Guinsoo's Rageblade");
+itemMap.set(786, "Runaan's Hurricane");
+itemMap.set(692, 'Statikk Shiv');
+itemMap.set(687, "Zz'Rot Portal");
+itemMap.set(805, 'Last Whisper');
+itemMap.set(1026, 'Duelist Emblem');
+itemMap.set(836, 'Bramble Vest');
+itemMap.set(711, 'Locket of the Iron Solari');
+itemMap.set(586, "Rabadon's Deathcap");
+itemMap.set(908, 'Gargoyle Stoneplate');
+itemMap.set(814, "Protector's Vow");
+itemMap.set(809, 'Sunfire Cape');
+itemMap.set(927, 'Shroud of Stillness');
+itemMap.set(1148, 'Ox Force Emblem');
+itemMap.set(783, 'Ionic Spark');
+itemMap.set(980, "Dragon's Claw");
+itemMap.set(886, 'Chalice of Power');
+itemMap.set(881, 'Zephyr');
+itemMap.set(999, 'Quicksilver');
+itemMap.set(1220, 'A.D.M.I.N. Emblem');
+itemMap.set(923, 'Spear of Shojin');
+itemMap.set(689, "Archangel's Staff");
+itemMap.set(787, 'Redemption');
+itemMap.set(792, 'Blue Buff');
+itemMap.set(905, 'Hand of Justice');
+itemMap.set(1126, 'Heart Emblem');
+itemMap.set(684, 'Morellonomicon');
+itemMap.set(782, "Warmog's Armor");
+itemMap.set(900, 'Guardbreaker');
+itemMap.set(1121, 'Mascot Emblem');
+itemMap.set(802, 'Jeweled Gauntlet');
+itemMap.set(1018, "Theif's Gloves");
+itemMap.set(1239, 'Renegade Emblem');
+itemMap.set(1023, 'Anima Squad Emblem');
 var x = new Array(59);
 
 
@@ -12,58 +56,13 @@ for (var i = 0; i < 59; i++) {
     x[i][0] = champList[i];
 }
 
-scrapeProduct("https://app.mobalytics.gg/tft/champions/");
+createItem("a", "a"); //for testing
 
-// itemMap.set('Deathblade', "Sword Sword");
-// itemMap.set('Giant Slayer', "Sword Bow");
-// itemMap.set('Edge of Night', "Sword Vest");
-// itemMap.set('Hextech Gunblade', "Sword Rod");
-// itemMap.set('Bloodthirster', "Sword Cloak");
-// itemMap.set("Zeke's Herald", "Sword Belt");
-// itemMap.set('Infinity Edge', "Sword Glove");
-// itemMap.set('LaserCorps Emblem', "Spatula Sword");
-// itemMap.set('Rapid Firecannon', "Bow Bow");
-// itemMap.set("Titan's Resolve", "Bow Vest");
-// itemMap.set("Guinsoo's Rageblade", "Bow Rod");
-// itemMap.set("Runaan's Hurricane", "Bow Cloak");
-// itemMap.set('Statikk Shiv', "Bow Tear");
-// itemMap.set("Zz'Rot Portal", "Bow Belt");
-// itemMap.set('Last Whisper', "Bow Glove");
-// itemMap.set('Duelist Emblem', "Bow Spatula");
-// itemMap.set('Bramble Vest', "Vest Vest");
-// itemMap.set('Locket of the Iron Solari', "Vest Rod");
-// itemMap.set("Rabadon's Deathcap", "Rod Rod");
-// itemMap.set('Gargoyle Stoneplate', "Vest Cloak");
-// itemMap.set("Protector's Vow", "Vest Tear");
-// itemMap.set('Sunfire Cape', "Vest Belt");
-// itemMap.set('Shroud of Stillness', "Vest Glove");
-// itemMap.set('Ox Force Emblem', "Vest Spatula");
-// itemMap.set('Ionic Spark', "Rod Cloak");
-// itemMap.set("Dragon's Claw", "Cloak Cloak");
-// itemMap.set('Chalice of Power', "Cloak Tear");
-// itemMap.set('Zephyr', "Cloak Belt");
-// itemMap.set('Quicksilver', "Cloak Glove");
-// itemMap.set('A.D.M.I.N. Emblem', "Cloak Spatula");
-// itemMap.set('Spear of Shojin', "Sword Tear");
-// itemMap.set("Archangel's Staff", "Rod Tear");
-// itemMap.set('Redemption', "Tear Belt");
-// itemMap.set('Blue Buff', "Tear Tear");
-// itemMap.set('Hand of Justice', "Tear Glove");
-// itemMap.set('Heart Emblem', "Tear Spatula");
-// itemMap.set('Morellonomicon', "Rod Belt");
-// itemMap.set("Warmog's Armor", "Belt Belt");
-// itemMap.set('Guardbreaker', "Belt Glove");
-// itemMap.set('Mascot Emblem', "Belt Spatula");
-// itemMap.set('Jeweled Gauntlet', "Rod Glove");
-// itemMap.set("Theif's Gloves", "Glove Glove");
-// itemMap.set('Renegade Emblem', "Glove Spatula");
-// itemMap.set('Anima Squad Emblem', "Rod Spatula");
+//scrapeProduct("https://app.mobalytics.gg/tft/champions/");
 
 
 
-
-
-async function scrapeProduct(url) {
+async function scrapeProduct(url) { //rewrite this with multithreading, its really slow right now. maybe that will help.
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
@@ -105,7 +104,7 @@ async function scrapeProduct(url) {
 }
 
 
-function startProgram() {
+function startProgram() { //this is where the fun begins
     console.log(x[0][2]);
     console.log(x[10][2]);
     console.log(x[20][2]);
@@ -113,8 +112,28 @@ function startProgram() {
 
 
 
+function createItem(itemOne, itemTwo) {
+    let tear = 396;
+    let bow = 296;
+    let glove = 509;
+    let vest = 418;
+    let cloak = 490;
+    let sword = 527;
+    let rod = 293;
+    let belt = 391;
+    let spat = 730;
+    let currIt = 0;
+    let x = new Array(396,296,509,418,490,527,293,391,730);
 
+    let y = new Array(45);
 
+    for (let j = 0; j < 9; j++) {
+        for (let r = j; r < 9; r++) {
+            y[currIt] = x[j] + x[r];
+            currIt++;
+        }
+    }
+}
 
 
 
@@ -139,5 +158,3 @@ function startProgram() {
 
 //     rl.close();
 // });
-
-
